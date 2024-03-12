7526b12bc645741f038cd06bd0e52615eea9c06d
Cauê Lima <cauesvlima@gmail.com>
2024-03-12 15:01:40 -0300

    comited

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import HelloWord from './components/HelloWord';
 import SayMyName from './components/SayMyName'; 
 import Pessoa from './components/Pessoa'; 
 import List from './components/List'; 
-import List from './components/List'; 
+import Evento from './components/Evento'; 
 function App() {
   return (
     <div className="App">
       <h1>Testando evento</h1>
-    </div>
+      <Evento />
+          </div>
   );
 }
 
