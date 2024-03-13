082a66840d86e414fdff6f6a524a04b33d143fa0
Cauê Lima <cauesvlima@gmail.com>
2024-03-13 18:51:04 -0300

    comito

--- src/components/Evento.js
+++ src/components/Evento.js
@@ -6,13 +6,16 @@ function meuEvento(){
     console.log(`Ativando primero evento`)
 }
 
+function segundoEvento(){
+    console.log("Ativando segundo evento")
+}
+
     return(
         <>
         <p>Clique para disparar um evento:</p>
         <br/>
-        
-      <Button event={meuEvento} text="Primeiro evento"/>
-
+        <Button event={meuEvento} text="Primeiro evento"/>
+        <Button event={segundoEvento} text="Segundo evento"/>
         </>
     )
 }
